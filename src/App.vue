810f0d66a772b2e7299b5c3a267b83023bb4d9a0
cminacardena <christopher.luis.mina.cardenas@students.iaac.net>
2024-01-28 17:35:29 -0500

    New imagines
    GIF

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import { ref } from "vue" // you need this to use ref()
 <template>
 
 <div id="navbar" class="container">  
-        <div id="title">David's bootcamp website website</div>
+        <div id="title">Christopher Mina  Website</div>
         
         <div id="logo">
             <img src="./cow.jpg">
@@ -27,13 +27,69 @@ import { ref } from "vue" // you need this to use ref()
 
         <div id="main" class="container"> Text input </div>
     </div>
-
+	<div id="navbar">
+		<button id="myButton">Click me!</button>
+	</div>
+	
+	<div id="flex">
+		<div id="sidebar">Sidebar</div>
+		<div id="main">Text input</div>
+	</div>
 </template>
 
+<button id="img-btn">Display Image</button>
 
 
 <!-- style is where the css code goes -->
 <style scoped>
-
+	html {
+			background-color: aqua;
+		}
+		
+		body {
+			margin: 0;
+		}
+		
+		div {
+			/* put the borders in the inside of container */
+			box-sizing: border-box;
+		}
+		
+		img {
+			height: 100%;
+			width: auto;
+		}
+		
+		#flex {
+			display: flex;
+			height: calc(100vh - 50px);
+		}
+		
+		#sidebar {
+			width: 40%;
+			border-color: blue;
+		}
+		
+		#main {
+			width: 70%;
+			background-color: green;
+		}
+		
+		#title {
+			width: 80%;
+			float: left;
+		}
+		
+		#logo {
+			width: 30%;
+			height: 100%;
+			float: right;
+			text-align: right;
+		}
+		
+		.container {
+			border-style: dotted;
+			border-width: 1px;
+		}
 
 </style>
